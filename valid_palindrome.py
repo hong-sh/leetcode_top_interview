201f4401a4b2d4c1acdd0348ebb5d2bfbc15e505
hong-sh <rkskekfk2020@naver.com>
2025-01-22 19:22:03 +0900

    valid palindrome

---
+++ valid_palindrome.py
@@ -0,0 +1,15 @@
+"""
+leetcode 125. Valid Palindrome
+https://leetcode.com/problems/valid-palindrome/description/?envType=study-plan-v2&envId=top-interview-150
+"""
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        pass
+
+
+sol = Solution()
+sol.isPalindrome(s="A man, a plan, a canal: Panama")
+sol.isPalindrome(s="race a car")
+sol.isPalindrome(s=" ")
